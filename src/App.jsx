876e2416781c97e95a23ee77b7e15b114839aec5
MatteoVentura <matteo.ventura18@gmail.com>
2025-05-27 11:13:33 +0200

    Milestone 2

--- src/App.jsx
+++ src/App.jsx
@@ -4,25 +4,28 @@ import { Route } from "react-router-dom";
 import { Routes } from "react-router-dom";
 import TaskList from "./pages/TaskList";
 import AddTask from "./pages/AddTask";
+import { GlobalProvider } from "./context/GlobalContext";
 
 function App() {
   return (
-    <BrowserRouter>
-      <nav className="navHeader">
-        <NavLink to="/">
-          {" "}
-          <h1 className="linkHeader">Task List</h1>
-        </NavLink>
-        <NavLink to="/add">
-          {" "}
-          <h1 className="linkHeader">Add Task</h1>
-        </NavLink>
-      </nav>
-      <Routes>
-        <Route path="/" element={<TaskList />} />
-        <Route path="/add" element={<AddTask />} />
-      </Routes>
-    </BrowserRouter>
+    <GlobalProvider>
+      <BrowserRouter>
+        <nav className="navHeader">
+          <NavLink to="/">
+            {" "}
+            <h1 className="linkHeader">Task List</h1>
+          </NavLink>
+          <NavLink to="/add">
+            {" "}
+            <h1 className="linkHeader">Add Task</h1>
+          </NavLink>
+        </nav>
+        <Routes>
+          <Route path="/" element={<TaskList />} />
+          <Route path="/add" element={<AddTask />} />
+        </Routes>
+      </BrowserRouter>
+    </GlobalProvider>
   );
 }
 
--- src/pages/TaskList.jsx
+++ src/pages/TaskList.jsx
@@ -1,4 +1,9 @@
+import { useContext } from "react";
+import { GlobalContext } from "../context/GlobalContext";
+
 export default function TaskList() {
+  const { tasks } = useContext(GlobalContext);
+  console.log("Tasks from context:", tasks);
   return (
     <div className="text-center pt-4">
       <h1>Task List</h1>
