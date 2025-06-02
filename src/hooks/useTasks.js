171fd84d1a9e743da8be44ebb796182e223dac7a
MatteoVentura <matteo.ventura18@gmail.com>
2025-06-02 10:36:51 +0200

    Milestone 6

--- src/hooks/useTasks.js
+++ src/hooks/useTasks.js
@@ -10,7 +10,19 @@ export default function useTasks() {
       .catch((error) => console.error("Error fetching tasks:", error));
   }, []);
 
-  const addTask = () => {};
+  const addTask = async (newTask) => {
+    const response = await fetch(`${VITE_API_URL}/tasks`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newTask),
+    });
+    const { success, message, task } = await response.json();
+    if (!success) throw new Error(message);
+
+    setTasks((prev) => [...prev, task]);
+  };
 
   const updateTask = () => {};
 
--- src/pages/AddTask.jsx
+++ src/pages/AddTask.jsx
@@ -1,8 +1,9 @@
-import { useState, useRef, useMemo } from "react";
-
+import { useState, useRef, useMemo, useContext } from "react";
+import { GlobalContext } from "../context/GlobalContext";
 const symbols = "!@#$%^&*()-_=+[]{}|;:'\\,.<>?/`~";
 
 export default function AddTask() {
+  const { addTask } = useContext(GlobalContext);
   const [taskTitle, setTaskTitle] = useState("");
   const descriptionRef = useRef();
   const statusRef = useRef();
@@ -14,7 +15,7 @@ export default function AddTask() {
     return "";
   }, [taskTitle]);
 
-  const handleSubmit = (event) => {
+  const handleSubmit = async (event) => {
     event.preventDefault();
     if (taskTitleError) return;
 
@@ -23,7 +24,16 @@ export default function AddTask() {
       description: descriptionRef.current.value,
       status: statusRef.current.value,
     };
-    console.log("New Task:", newTask);
+
+    try {
+      await addTask(newTask);
+      alert("Task aggiunta con successo!");
+      setTaskTitle("");
+      descriptionRef.current.value = "";
+      statusRef.current.value = ""; // Reset to default status
+    } catch (error) {
+      alert(`Errore durante l'aggiunta della task: ${error.message}`);
+    }
   };
 
   return (
